c565227879d279335eaa71fa9c7ba37d264eabbe
Arturo <garciaarturo397@gmail.com>
2025-09-05 23:12:07 -0600

    fix(input): Agrega validacion en inputs

--- lib/components/input_base.dart
+++ lib/components/input_base.dart
@@ -3,18 +3,38 @@ import 'package:flutter/material.dart';
 class InputBase extends StatelessWidget {
   final String placeholder;
   final TextEditingController? controller;
+  final String? Function(String?)? validator;
+  final TextInputType? keyboardType;
+  final bool requerido;
 
-  const InputBase({super.key, required this.placeholder, this.controller});
+  const InputBase({
+    super.key,
+    required this.placeholder,
+    this.controller,
+    this.validator,
+    this.keyboardType,
+    this.requerido = false,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
       controller: controller,
+      keyboardType: keyboardType,
       decoration: InputDecoration(
         hintText: placeholder,
-        contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 12),
+        contentPadding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
       ),
+      validator: (value) {
+        if (requerido && (value == null || value.isEmpty)) {
+          return 'Campo requerido';
+        }
+        if (validator != null) {
+          return validator!(value);
+        }
+        return null;
+      },
     );
   }
 }
